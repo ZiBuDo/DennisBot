a8ab78c505af0fb0839ca293473e10b5ace18538
Paul <pgregotski@gmail.com>
2018-07-18 21:22:46 -0400

    Register user

--- src/server/controller/message.ts
+++ src/server/controller/message.ts
@@ -10,15 +10,15 @@ export default async function message(session: any) {
     // if a command then go to command flow
     // if not a command then determine if we are triggered
     // if triggered then go to trigger + action flow
-    console.log(JSON.stringify(session.message.user));
     const userRepo = getEntityManager().getRepository(User);
 
-    // // see if we can find user
-    // const user = await userRepo.findOne({source: ''});
-    // if (!user) {
-    //     // need to register
-
-    // }else {
-    //     // parse into command or trigger if neither do nothing
-    // }
+    // see if we can find user
+    const user = await userRepo.findOne({source: session.message.user.id});
+    if (!user) {
+        // need to register: ask for name
+        userRepo.create({name: session.message.user.name, source: session.message.user.id});
+        session.send('Nice to meet you ' + session.message.user.name);
+    }else {
+        // parse into command or trigger if neither do nothing
+    }
 }
