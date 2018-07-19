33c2b4dda85abb171e4e528f3dda69b1c49cb03b
Paul <pgregotski@gmail.com>
2018-07-18 21:56:52 -0400

    updated persisting

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 cp ../server_config/apikey.txt ./src/server/
 cp ../server_config/certPath.txt ./src/server/
-cp ../server_config/ormconfig.json ./src/server/
+cp ../server_config/ormconfig.json .
 cp ../server_config/keyPath.txt ./src/server/
